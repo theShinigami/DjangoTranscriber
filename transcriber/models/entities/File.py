e8f7910202be0fee81cb3034e73f21c8504405fe
masterSal <salimabdellanuru@gmail.com>
2023-10-21 22:26:53 +0300

    add: File entity added File entity

---
+++ transcriber/models/entities/File.py
@@ -0,0 +1,22 @@
+from django.db import models
+from uuid import uuid4
+
+
+class File(models.Model):
+    """file field"""
+    file = models.FileField()
+    """alternate file id"""
+    file_id = models.UUIDField(default=uuid4())
+    """flag to check if file is transcribed or not"""
+    is_transcribed = models.BooleanField(default=False)
+    # TODO: change this to a file
+    """transcribed text"""
+    transcribed_text = models.TextField(null=True)
+    """tracks last update"""
+    last_update = models.DateTimeField(auto_now=True)
+    """created date"""
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "files"
+        app_label = "transcriber"
