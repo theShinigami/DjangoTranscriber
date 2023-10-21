815c5ec4770d7996a4eef2991e1912f9171885aa
masterSal <salimabdellanuru@gmail.com>
2023-10-21 22:25:53 +0300

    update: model register registered File model

--- transcriber/admin.py
+++ transcriber/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# model
+from transcriber.models.entities.File import File
+
+
+@admin.register(File)
+class FilesAdmin(admin.ModelAdmin):
+    list_display = ("file_id", "file", "is_transcribed", "last_update", "created_at",)
+
+
+
