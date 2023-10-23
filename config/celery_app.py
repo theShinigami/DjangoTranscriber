4ae5ae16b885bbd5df603e67c5bac82f28a77989
masterSal <salimabdellanuru@gmail.com>
2023-10-23 21:26:53 +0300

    update: celery app config changed celery name, configs [result_expires, task of time limit, tas time limit] and debug task

--- config/celery_app.py
+++ config/celery_app.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-app = Celery("transcriber")
+app = Celery("dj_transcriber")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -15,3 +15,14 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+# backend redis ttl
+app.conf.result_expires = 1800
+
+# time limits
+app.conf.update(task_soft_time_limit=300, task_time_limit=300)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
