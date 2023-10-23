1f58041b80c7f74817980c92f0ae39e7747b7e3c
masterSal <salimabdellanuru@gmail.com>
2023-10-23 21:30:10 +0300

    update: view updates TranscriberView added file_id on serializer save, TranscribeQueueView for handling transcribe celery queue, TranscribedFetch fetch transcribed task

--- transcriber/views/transcribe_view.py
+++ transcriber/views/transcribe_view.py
@@ -1,5 +1,6 @@
 # encoding=utf-8
 import logging
+from uuid import uuid4
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -46,7 +47,7 @@ class TranscribeView(APIView):
                     errors.append("invalid file type")
                     response_status = status.HTTP_400_BAD_REQUEST
                 else:
-                    result = serialized.save()
+                    result = serialized.save(file_id=uuid4())
                     file_id = str(result.file_id)
                     transcribed = self.transcriber_service.transcribe_now(file_id)
                     res = ResponseModel(data=transcribed.dict(), success=True)
@@ -60,3 +61,64 @@ class TranscribeView(APIView):
         if len(errors) > 0:
             response = ResponseModel(data={"error": errors}, success=False)
             return Response(data=ResponseSerializer(response).data, status=response_status)
+
+
+class TranscribeQueueView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = TranscribeSerializer
+    transcriber_service = TranscriberService()
+    whitelist_content_type = ["audio/mpeg"]
+
+    def post(self, request, *args, **kwargs):
+        errors = list()
+        response_status = status.HTTP_200_OK
+
+        # check if request data is not empty
+        if request.data is None:
+            logger.error("[TranscribeQueueView]: invalid request body")
+
+            errors.append("invalid request body")
+            response_status = status.HTTP_400_BAD_REQUEST
+
+        if len(errors) == 0:
+            serialized = self.serializer_class(data=request.data)
+
+            if serialized.is_valid():
+                file = request.FILES.get("file")
+                if file.content_type not in self.whitelist_content_type:
+                    logger.error("[TranscribeQueueView]: invalid content type: %s", file.content_type)
+                    errors.append("invalid file type")
+                    response_status = status.HTTP_400_BAD_REQUEST
+                else:
+                    result = serialized.save(file_id=uuid4())
+                    file_id = str(result.file_id)
+                    transcribed = self.transcriber_service.transcribe_queue(file_id=file_id)
+                    res = ResponseModel(data=transcribed.dict(), success=True)
+                    return Response(data=ResponseSerializer(res).data, status=response_status)
+            else:
+                logger.error("[TranscribeQueueView]: invalid request data!")
+
+                errors.append("bad request")
+                response_status = status.HTTP_400_BAD_REQUEST
+
+        if len(errors) > 0:
+            response = ResponseModel(data={"error": errors}, success=False)
+            return Response(data=ResponseSerializer(response).data, status=response_status)
+
+
+class TranscribedFetch(APIView):
+    permission_classes = (AllowAny,)
+    transcriber_service = TranscriberService()
+
+    def get(self, request, *args, **kwargs):
+        try:
+            file_id = kwargs['id']
+            result = self.transcriber_service.get_transcribed_audio(file_id=file_id)
+            response = ResponseModel(data=result.dict(), success=True)
+            return Response(data=ResponseSerializer(response).data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error("[TranscribedFetch]: unable to fetch transcribed file")
+            logger.error("[TranscribedFetch]: %s", str(e))
+
+        response = ResponseModel(data={"error": True}, success=False)
+        return Response(data=ResponseSerializer(response).data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
