11ac8543e98a31b9c73e608e606fd982de92ff42
masterSal <salimabdellanuru@gmail.com>
2023-10-23 21:33:05 +0300

    update: more urls transcribe queue path and get path

--- transcriber/urls.py
+++ transcriber/urls.py
@@ -2,8 +2,10 @@
 from django.urls import re_path
 
 # views
-from transcriber.views.transcribe_view import TranscribeView
+from transcriber.views.transcribe_view import TranscribeView, TranscribedFetch, TranscribeQueueView
 
 urlpatterns = [
     re_path("^transcribe/$", TranscribeView.as_view()),
+    re_path("^transcribe/queue/$", TranscribeQueueView.as_view()),
+    re_path("^get/(?P<id>[0-9a-f-]+)$", TranscribedFetch.as_view()),
 ]
