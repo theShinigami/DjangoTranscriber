12e63e8b7019913a15ff25b42234800ed9573803
masterSal <salimabdellanuru@gmail.com>
2023-10-23 21:32:34 +0300

    update: more exception class QueueTranscribeError and TranscribeDataFetchError

--- transcriber/exceptions/transcriber_service_exception.py
+++ transcriber/exceptions/transcriber_service_exception.py
@@ -5,3 +5,13 @@ from transcriber.exceptions.base import TranscriberServiceBaseException
 class InstantTranscribeError(TranscriberServiceBaseException):
     """throws if it encounters an error while transcribing"""
     pass
+
+
+class QueueTranscribeError(TranscriberServiceBaseException):
+    """throws if it encounters an error while queuing task"""
+    pass
+
+
+class TranscribeDataFetchError(TranscriberServiceBaseException):
+    """throws if an error encounters while fetching transcribed file"""
+    pass
