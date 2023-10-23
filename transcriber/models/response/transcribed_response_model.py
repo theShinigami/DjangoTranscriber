bf763ff2a86e652201aaa5e1cbfadaa623cbf00e
masterSal <salimabdellanuru@gmail.com>
2023-10-23 21:31:07 +0300

    update: added more fields transcribed flag

--- transcriber/models/response/transcribed_response_model.py
+++ transcriber/models/response/transcribed_response_model.py
@@ -2,15 +2,18 @@
 
 
 class Transcribed:
-    def __init__(self, text: str, duration: float):
+    def __init__(self, text: str, transcribed: bool, duration: float):
         """
         transcribed model
         :param text: transcribed model
         :type text: str
+        :param transcribed: transcribe indicator
+        :type transcribed: bool
         :param duration: time taken
         :type duration: float
         """
         self.text = str(text)
+        self.transcribed = transcribed
         self.duration = duration
 
     def dict(self):
