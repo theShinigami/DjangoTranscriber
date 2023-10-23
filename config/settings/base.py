ed284cb5f0ebb262a26f0102ba7003aa62314e3c
masterSal <salimabdellanuru@gmail.com>
2023-10-23 21:25:14 +0300

    update: celery config set flag to true CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP

--- config/settings/base.py
+++ config/settings/base.py
@@ -255,6 +255,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_WORKER_SEND_TASK_EVENTS = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std-setting-task_send_sent_event
 CELERY_TASK_SEND_SENT_EVENT = True
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 # django-allauth
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
